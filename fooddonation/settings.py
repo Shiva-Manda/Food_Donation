a8808c9a0d96a8239bd59ed30b87db511e249a4d
Shiva-Manda <shivamanda2408@gmail.com>
2025-06-17 10:20:13 +0530

    Initial commit

--- fooddonation/settings.py
+++ fooddonation/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Environment-based security settings
 SECRET_KEY = os.environ.get('SECRET_KEY', 'insecure-default-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = [*]
 
 # Installed applications
 INSTALLED_APPS = [
@@ -66,8 +66,9 @@ WSGI_APPLICATION = 'fooddonation.wsgi.application'
 # Database: Uses MySQL if DATABASE_URL is set, else falls back to SQLite
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600
+        default=os.environ.get('postgresql://mydb_wk4x_user:jJVkrhMIMrOQbUrhG7LNvIAqzxqGox5c@dpg-d18eusodl3ps7389kvv0-a/mydb_wk4x'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
@@ -89,7 +90,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'donateapp/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+
 
 # Authentication
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
