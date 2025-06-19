325ecf9dc3a5900b306accbb08372f27f79274db
Shiva-Manda <shivamanda2408@gmail.com>
2025-06-19 20:53:30 +0530

    Update urls.py

--- fooddonation/urls.py
+++ fooddonation/urls.py
@@ -18,4 +18,4 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='donateapp/password_reset_confirm.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='donateapp/password_reset_complete.html'), name='password_reset_complete'),
 ]
-import donateapp.create_superuser
+
