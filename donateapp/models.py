493a6cf25bb03ece0f48438f7b9298d15657caa9
Shiva-Manda <shivamanda2408@gmail.com>
2025-06-20 17:44:26 +0530

    Update models.py

--- donateapp/models.py
+++ donateapp/models.py
@@ -28,6 +28,8 @@ class Notification(models.Model):
         User, related_name="notification", on_delete=models.CASCADE
     )
     notification = models.CharField(max_length=500, default=None)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_seen = models.BooleanField(default=False) 
     
 class FoodAcceptor(models.Model):
     STATUS_CHOICES = [
