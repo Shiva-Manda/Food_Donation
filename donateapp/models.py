5407c1fdfd4a37ca699137d4add69c4583db5ccf
Shiva-Manda <shivamanda2408@gmail.com>
2025-06-20 20:24:43 +0530

    Update models.py

--- donateapp/models.py
+++ donateapp/models.py
@@ -3,7 +3,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone 
 from datetime import timedelta
+from django.utils.timezone import now
 
+def default_expiry():
+    return now() + timedelta(days=1)
 
 
 
