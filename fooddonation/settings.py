1ddcc53848bf000c1b350cf1ad50de849444da49
Shiva-Manda <shivamanda2408@gmail.com>
2025-06-20 14:11:18 +0530

    Update settings.py

--- fooddonation/settings.py
+++ fooddonation/settings.py
@@ -100,7 +100,7 @@ if not DEBUG:
 # Authentication
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'home_authenticated/'
+LOGIN_REDIRECT_URL = 'home_authenticated'
 LOGOUT_REDIRECT_URL = '/'
 
 # Email backend (for development)
