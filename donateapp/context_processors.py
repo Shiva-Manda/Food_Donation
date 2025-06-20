7b20987b45a849681cc65a4e4246a47b663bd8d3
Shiva-Manda <shivamanda2408@gmail.com>
2025-06-20 18:12:57 +0530

    Update context_processors.py

--- donateapp/context_processors.py
+++ donateapp/context_processors.py
@@ -1,11 +1,8 @@
-from .models import Notification
-from django.contrib.auth.models import AnonymousUser
+from .models import FoodAcceptor, Notification
 
-def unread_notifications(request):
-    if request.user == AnonymousUser() or not request.user.is_authenticated:
-        return {}
-
-    unread_count = Notification.objects.filter(user=request.user, is_seen=False).count()
-    return {
-        'unread_notifications_count': unread_count
-    }
+def notification_count(request):
+    if request.user.is_authenticated:
+        donor_count = FoodAcceptor.objects.filter(donation__user=request.user, status="Pending").count()
+        acceptor_count = Notification.objects.filter(user=request.user).count()
+        return {"donor_notification_count": donor_count, "acceptor_notification_count": acceptor_count}
+    return {}
