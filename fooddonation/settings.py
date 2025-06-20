e738c1658ea235e584b874c38b3ce38fd247388e
Shiva-Manda <shivamanda2408@gmail.com>
2025-06-20 11:02:44 +0530

    Update settings.py

--- fooddonation/settings.py
+++ fooddonation/settings.py
@@ -88,7 +88,7 @@ USE_TZ = True
 
 # Static files (CSS, JS, etc.)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'donateapp/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage',
 if not DEBUG:
