87167663d4965abe57164d23478bacb44f05d26e
Shiva-Manda <shivamanda2408@gmail.com>
2025-06-20 12:29:53 +0530

    Update urls.py

--- fooddonation/urls.py
+++ fooddonation/urls.py
@@ -6,6 +6,7 @@ from donateapp import views as donate_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('donateapp.urls')),
+    path('',views.home,name='home'),
 
    
     path('login/', auth_views.LoginView.as_view(template_name='donateapp/login.html'), name='login'),
