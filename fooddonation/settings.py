8c6db6beee78b8dde014a6e101301ce1ddbe4e2e
Shiva-Manda <shivamanda2408@gmail.com>
2025-06-20 18:09:29 +0530

    Update settings.py

--- fooddonation/settings.py
+++ fooddonation/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Environment-based security settings
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 # Installed applications
