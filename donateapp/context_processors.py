53675a246581284424887f86095b10ec0b9b9085
Shiva-Manda <shivamanda2408@gmail.com>
2025-06-20 17:43:24 +0530

    Update context_processors.py

--- donateapp/context_processors.py
+++ donateapp/context_processors.py
@@ -1,6 +1,11 @@
 from .models import Notification
+from django.contrib.auth.models import AnonymousUser
 
-def notification_count(request):
-    if request.user.is_authenticated:
-        return {'noti_count': Notification.objects.filter(user=request.user).count()}
-    return {'noti_count': 0}
+def unread_notifications(request):
+    if request.user == AnonymousUser() or not request.user.is_authenticated:
+        return {}
+
+    unread_count = Notification.objects.filter(user=request.user, is_seen=False).count()
+    return {
+        'unread_notifications_count': unread_count
+    }
